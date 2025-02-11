ad10601f7dc1b09082c01608cd0f83519011d4d0
Donovan Preston <46048+fzzzy@users.noreply.github.com>
2025-02-11 17:08:51 -0500

    Add some useful responses.

--- python/mumulib/mumutypes.py
+++ python/mumulib/mumutypes.py
@@ -7,6 +7,7 @@ class SpecialResponse(Exception):
         self.writer = writer
 
 
+
 class HTTPResponse(SpecialResponse):
     def __init__(self, code, body):
         SpecialResponse.__init__(self, {
@@ -16,4 +17,36 @@ class HTTPResponse(SpecialResponse):
                         (b'content-type', b'text/plain'),
                     ],
         }, body)
+
+
+class BadRequestResponse(HTTPResponse):
+    def __init__(self):
+        HTTPResponse.__init__(self, 400, 'Bad Request')
+
+
+class NotFoundResponse(HTTPResponse):
+    def __init__(self):
+        HTTPResponse.__init__(self, 404, 'Not Found')
+
+
+class MethodNotAllowedResponse(HTTPResponse):
+    def __init__(self):
+        HTTPResponse.__init__(self, 405, 'Method Not Allowed')
+
+
+class CreatedResponse(HTTPResponse):
+    def __init__(self):
+        HTTPResponse.__init__(self, 201, 'Created')
+
+
+class SeeOtherResponse(SpecialResponse):
+    def __init__(self, redirect_to):
+        SpecialResponse.__init__(self, {
+                    'type': 'http.response.start',
+                    'status': 303,
+                    'headers': [
+                        (b'content-type', b'application/json'),
+                        (b'location', redirect_to.encode('utf8')),
+                    ],
+        }, '')
 
